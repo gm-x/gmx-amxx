4db867d4385dec4bd826336ce2d4f2b1c5d2f08d
fant1kua <fant1kua@users.noreply.github.com>
2020-05-19 01:21:20 +0300

    Bump version

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ cat > scripting/include/gmx_version.inc <<EOT
 #define GMX_MAJOR_VERSION			0
 #define GMX_MINOR_VERSION			1
 #define GMX_MAINTENANCE_VERSION		$version
-#define GMX_VERSION_STR				"0.1.$version"
+#define GMX_VERSION_STR				"0.1.$version-alpha"
 EOT
 
 zip -9 -r -q --exclude=".git/*" --exclude=".gitignore" --exclude=".gitkeep" --exclude=".travis.yml" --exclude="README.md" --exclude="deploy.sh" gmx-amxx.zip .
